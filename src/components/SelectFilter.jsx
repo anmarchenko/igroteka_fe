7cb59d4e34fa16ef6de0240de7209bc6742998fc
Andrey Marchenko <igendou@gmail.com>
2020-04-21 21:30:46 +0200

    Redesign selects

--- src/components/SelectFilter.jsx
+++ src/components/SelectFilter.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {
-  UncontrolledButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem,
-} from 'reactstrap';
-
 const selectImportantOptions = (options, important) => options.filter((o) => important.includes(o.label));
 
 const selectRestOptions = (options, important) => options.filter((o) => !important.includes(o.label));
 
-const renderItems = (options, onChange) => {
+const renderItems = (options) => {
   if (!options || options.length === 0) {
     return null;
   }
   return options.map((option) => (
-    <DropdownItem key={option.value} onClick={() => onChange(option.value)}>
+    <option key={option.value} value={option.value}>
       {option.label}
-    </DropdownItem>
+    </option>
   ));
 };
 
@@ -31,10 +27,6 @@ export const SelectFilter = ({
   if (!options || options.length === 0) {
     return <span />;
   }
-  const selectedItem = selectedValue
-    ? options.find((opt) => opt.value.toString() === selectedValue.toString())
-    : null;
-
   const important = selectImportantOptions(options, importantOptions);
   const rest = selectRestOptions(options, importantOptions);
 
@@ -43,19 +35,15 @@ export const SelectFilter = ({
       <div className="form-group">
         <label htmlFor="backlog_owned_platform_filter">{label}</label>
         <br />
-        <UncontrolledButtonDropdown>
-          {!selectedItem && <DropdownToggle caret>Select...</DropdownToggle>}
-          {selectedItem && <DropdownToggle caret>{selectedItem.label}</DropdownToggle>}
-          <DropdownMenu>
-            {selectedItem && (
-              <DropdownItem onClick={() => onChange(null)}>{clearFilterLabel}</DropdownItem>
-            )}
-            {selectedItem && <DropdownItem divider />}
-            {renderItems(important, onChange)}
-            {important.length > 0 && rest.length > 0 && <DropdownItem divider />}
-            {renderItems(rest, onChange)}
-          </DropdownMenu>
-        </UncontrolledButtonDropdown>
+        <select
+          className="form-control"
+          value={selectedValue || ''}
+          onChange={(e) => onChange(e.target.value)}
+        >
+          <option value="">{clearFilterLabel}</option>
+          {renderItems(important)}
+          {renderItems(rest)}
+        </select>
       </div>
     </div>
   );
