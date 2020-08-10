eea5353d670b03a4de0f032d119fcbd9fb96f2fd
Andrey Marchenko <igendou@gmail.com>
2020-08-10 09:29:51 +0200

    disable service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-image-gallery/styles/css/image-gallery.css';
@@ -17,3 +18,8 @@ axios.interceptors.request.use((config) => {
 });
 
 ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line react/jsx-filename-extension
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
